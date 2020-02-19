adb412e9e60c607e0d73d647e36249420184389c
rileymprice <rileymprice@gmail.com>
2020-02-19 14:40:23 -0600

    Update two_step_dominance.py
    Completed basic functionality of two step dominance.

--- two_step_dominance.py
+++ two_step_dominance.py
@@ -7,52 +7,52 @@ from tqdm import tqdm
 LEAGUE_ID = os.environ.get("SLEEPER_LEAGUE_ID")
 LEAGUE = sleeper_wrapper.League(LEAGUE_ID)
 MAX_WEEKS = 13
-for week in tqdm(range(MAX_WEEKS + 1)):
-    weekly_matchups = LEAGUE.get_matchups(week)
+TEAM_COUNT = 10
 
 
-team_count = 10
+def two_step(league_id, max_weeks):
+    """Takes league ID and max weeks and provides a dictionary of roster IDs and two step dominance sum"""
+    total_results = []
+    for week in tqdm(range(1, MAX_WEEKS + 1)):
+        weekly_matchups = LEAGUE.get_matchups(week)
+        # print(weekly_matchups)
+        matchup_results = {}
+        for team in weekly_matchups:
+            roster_id = team["roster_id"]
+            points = team["points"]
+            matchup_id = team["matchup_id"]
+            if matchup_id in matchup_results:
+                matchup_results[matchup_id].append({"team": roster_id, "score": points})
+            else:
+                matchup_results[matchup_id] = [{"team": roster_id, "score": points}]
+        for matchup in matchup_results.values():
+            if matchup[0]["score"] > matchup[1]["score"]:
+                winner_loser = (matchup[0]["team"], matchup[1]["team"])
+            else:
+                winner_loser = (matchup[1]["team"], matchup[0]["team"])
+            total_results.append(winner_loser)
 
-winning = [
-    (1, 3),
-    (2, 5),
-    (9, 7),
-    (3, 6),
-    (3, 1),
-    (9, 2),
-    (9, 8),
-    (10, 6),
-    (1, 10),
-    (1, 2),
-    (2, 6),
-    (9, 6),
-    (7, 6),
-]
-team_array = np.zeros((team_count, team_count), dtype=int)
-team_final = {}
-for winning_team, losing_team in np.ndindex(team_array.shape):
-    if (winning_team + 1, losing_team + 1) in winning:
-        team_array[winning_team, losing_team] = 1
-square_team_array = np.matmul(team_array, team_array)
-team_index = 1
-for row in team_array:
-    for value in row:
-        # print(value)
-        if team_index in team_final.keys():
-            team_final[team_index] += value
-        else:
-            team_final[team_index] = value
-    team_index += 1
-team_index = 1
-for row in square_team_array:
-    for value in row:
-        if team_index in team_final.keys():
-            team_final[team_index] += value
-        else:
-            team_final[team_index] = value
-    team_index += 1
-print(team_final)
-# print(team_array)
-print(square_team_array)
-# print(team_final)
-
+    team_array = np.zeros((TEAM_COUNT, TEAM_COUNT), dtype=int)
+    team_final = {}
+    for winning_team, losing_team in np.ndindex(team_array.shape):
+        if (winning_team + 1, losing_team + 1) in total_results:
+            team_array[winning_team, losing_team] = 1
+    square_team_array = np.matmul(team_array, team_array)
+    team_index = 1
+    for row in team_array:
+        for value in row:
+            # print(value)
+            if team_index in team_final.keys():
+                team_final[team_index] += value
+            else:
+                team_final[team_index] = value
+        team_index += 1
+    team_index = 1
+    for row in square_team_array:
+        for value in row:
+            if team_index in team_final.keys():
+                team_final[team_index] += value
+            else:
+                team_final[team_index] = value
+        team_index += 1
+    return team_final
